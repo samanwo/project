c73497aa1f49b7c935edacbdd6aacee20fbdd0fc
samuel anwo <moroti95@yahoo.com>
2019-12-14 14:14:31 -0600

    Initial commit

---
+++ app/src/main/java/com/hussein/startup/StartWithOS.kt
@@ -0,0 +1,17 @@
+package com.hussein.startup
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+
+class  StartWithOS:BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        if (intent!!.action.equals("android.intent.action.BOOT_COMPLETED")){
+            val intent= Intent(context,MyService::class.java)
+            context!!.startService(intent)
+        }
+    }
+
+}
